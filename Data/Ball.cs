acc42fd329f80fd6959a35db0f8fb10e8033092c
Jakub Rosiak <251620@edu.p.lodz.pl>
2025-03-14 21:05:45 +0100

    feat: add a basic class for balls

---
+++ Data/Ball.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class Ball
+    {
+        public static readonly double Radius = 5;
+        public double XPos { get; private set; }
+        public double YPos { get; private set; }
+        public double XVel { get; private set; }
+        public double YVel { get; private set; }
+        public Ball(double xPos, double yPos, double xVel, double yVel)
+        {
+            XPos = xPos;
+            YPos = yPos;
+            XVel = xVel;
+            YVel = yVel;
+        }
+
+        public void Move(double deltaTime)
+        {
+            XPos += XVel * deltaTime;
+            YPos += YVel * deltaTime;
+        }
+
+        public void AddForce(double xVel, double yVel)
+        {
+            XVel += xVel;
+            YVel += yVel;
+        }
+    }
+}
