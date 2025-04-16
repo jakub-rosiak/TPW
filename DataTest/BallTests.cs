14ee5304742ed46b654c2b9551fa2a8e83a971d2
Krzysztof Miller <251591@edu.p.lodz.pl>
2025-04-16 12:16:48 +0200

    test

--- DataTest/BallTests.cs
+++ DataTest/BallTests.cs
@@ -17,10 +17,25 @@ namespace DataTest
         {
             var ball = new Ball(5, 10, 2, 3);
 
+            Assert.IsNotNull(ball);
             Assert.AreEqual(5, ball.XPos);
             Assert.AreEqual(10, ball.YPos);
             Assert.AreEqual(2, ball.XVel);
             Assert.AreEqual(3, ball.YVel);
+        }
+
+        [TestMethod]
+        public void test12()
+        {
+            var ball1 = new Ball(5, 10, 2, 3);
+            var ball2 = new Ball(5, 10, 2, 3);
+
+            Assert.AreEqual(ball1.XPos, ball2.XPos);
+            Assert.AreEqual(ball1.YPos, ball2.YPos);
+            Assert.AreEqual(ball1.XVel, ball2.XVel);
+            Assert.AreEqual(ball1.YVel, ball2.YVel);
+
+            Assert.AreNotSame(ball1, ball2);
         }
 
         [TestMethod]
