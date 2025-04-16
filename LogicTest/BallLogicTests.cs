1e4d8815973d70540109bd54eef11150017dd9ea
Jakub Rosiak <251620@edu.p.lodz.pl>
2025-04-16 23:22:58 +0200

    test: create logic tests

---
+++ LogicTest/BallLogicTests.cs
@@ -0,0 +1,19 @@
+﻿using Data;
+using Logic;
+
+namespace LogicTest
+{
+    [TestClass]
+    public sealed class BallLogicTests
+    {
+        [TestMethod]
+        public void Constructor_WhenBallLogicIsCreated_ShouldInitializeValuesCorrectly()
+        {
+            var repository = new BallsRepository();
+            var board = new Board(800, 600);
+            var logic = new BallLogic(repository, board);
+            Assert.IsNotNull(logic);
+            
+        }
+    }
+}
---
+++ LogicTest/BallMovedEventArgsTest.cs
@@ -0,0 +1,19 @@
+﻿using Data;
+using Logic;
+
+namespace LogicTest
+{
+    [TestClass]
+    public sealed class BallMovedEventArgsTest
+    {
+        [TestMethod]
+        public void Constructor_WhenBallMovedEventArgsIsCreated_ShouldInitializeValuesCorrectly()
+        {
+            var eventargs = new BallMovedEventArgs(1, 2, 3);
+            Assert.IsNotNull(eventargs);
+            Assert.AreEqual(1, eventargs.BallId);
+            Assert.AreEqual(2, eventargs.NewX);
+            Assert.AreEqual(3, eventargs.NewY);
+        }
+    }
+}
