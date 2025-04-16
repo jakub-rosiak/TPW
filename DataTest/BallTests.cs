04c5ddb3fa3367a3eb55a00232271139d49e9cca
Jakub Rosiak <251620@edu.p.lodz.pl>
2025-04-16 23:22:32 +0200

    test: update data tests

--- DataTest/BallTests.cs
+++ DataTest/BallTests.cs
@@ -10,24 +10,25 @@ namespace DataTest
     [TestClass]
     public class BallTests
     {
-        private Ball _ball = new Ball(0,0,0,0);
-        
         [TestMethod]
         public void Constructor_WhenBallIsCreated_ShouldInitializeValuesCorrectly()
         {
             var ball = new Ball(5, 10, 2, 3);
 
             Assert.IsNotNull(ball);
-            Assert.AreEqual(5, ball.XPos);
-            Assert.AreEqual(10, ball.YPos);
+            Assert.AreEqual(5, ball.Id);
+            Assert.AreEqual(10, ball.XPos);
+            Assert.AreEqual(2, ball.YPos);
+            Assert.AreEqual(3, ball.Radius);
         }
 
         [TestMethod]
         public void Constructor_WhenBallsAreCreated_ShouldNotBeSame()
         {
             var ball1 = new Ball(5, 10, 2, 3);
-            var ball2 = new Ball(5, 10, 2, 3);
-
+            var ball2 = new Ball(6, 10, 2, 3);
+            
+            Assert.AreNotEqual(ball1.Id, ball2.Id);
             Assert.AreEqual(ball1.XPos, ball2.XPos);
             Assert.AreEqual(ball1.YPos, ball2.YPos);
             Assert.AreEqual(ball1.Radius, ball2.Radius);
---
+++ DataTest/BoardTests.cs
@@ -0,0 +1,17 @@
+﻿using Data;
+
+namespace DataTest;
+
+[TestClass]
+public class BoardTests
+{
+    [TestMethod]
+    public void Constructor_WhenBoardIsCreated_InitializeValuesCorrectly()
+    {
+        var board = new Board(800, 600);
+        
+        Assert.IsNotNull(board);
+        Assert.AreEqual(800, board.Width);
+        Assert.AreEqual(600, board.Height);
+    }
+}
