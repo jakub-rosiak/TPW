8e0c13186713bf5159bf881518155a8838087990
Jakub Rosiak <251620@edu.p.lodz.pl>
2025-04-16 23:22:48 +0200

    feat: update ball logic

--- Logic/Interfaces/IBallLogic.cs
+++ Logic/Interfaces/IBallLogic.cs
@@ -3,7 +3,7 @@ namespace Logic
     public interface IBallLogic
     {
         event EventHandler<BallMovedEventArgs> BallMoved; 
-        void CreateBalls(int initialCount, double width, double height);
+        void CreateBalls(int initialCount);
         IEnumerable<Data.Ball> GetBalls();
         void Start(double intervalInMilliseconds);
         void Stop();
--- Logic/Services/BallLogic.cs
+++ Logic/Services/BallLogic.cs
@@ -8,31 +8,28 @@ namespace Logic
     {
         private readonly IBallsRepository _repository;
         private readonly Random _random = new();
+        private readonly Board _board;
         private Timer? _timer;
-
-        private double _boardWidth;
-        private double _boardHeight;
         
         public event EventHandler<BallMovedEventArgs>? BallMoved;
 
-        public BallLogic(IBallsRepository repository)
+        public BallLogic(IBallsRepository repository, Board board)
         {
             _repository = repository;
+            _board = board;
         }
 
-        public void CreateBalls(int initialCount, double width, double height)
+        public void CreateBalls(int initialCount)
         {
-            _boardWidth = width;
-            _boardHeight = height;
-
+            _repository.Clear();
             double radius = 10;
             for (int i = 0; i < initialCount; i++)
             {
                 Console.WriteLine($"Creating ball at ({i + 1}/{initialCount})");
-                double xPos = _random.NextDouble() * (width - 2 * radius) + radius;
-                double yPos = _random.NextDouble() * (height - 2 * radius) + radius;
+                double xPos = _random.NextDouble() * (_board.Width - 2 * radius) + radius;
+                double yPos = _random.NextDouble() * (_board.Height - 2 * radius) + radius;
 
-                Console.WriteLine($"Creating ball at ({xPos}/{yPos})");
+                Console.WriteLine($"Creating ball at ({xPos}, {yPos})");
                 var ball = new Ball(i, xPos, yPos, radius);
                 _repository.AddBall(ball);
             }
@@ -42,12 +39,11 @@ namespace Logic
         {
             foreach (var ball in _repository.GetAllBalls())
             {
-                Console.WriteLine("Updating ball positions");
                 double dx = _random.NextDouble() * 10 - 5;
                 double dy = _random.NextDouble() * 10 - 5;
                 
-                ball.XPos = Clamp(ball.XPos + dx, ball.Radius, _boardWidth - ball.Radius);
-                ball.YPos = Clamp(ball.XPos + dy, ball.Radius, _boardHeight - ball.Radius);
+                ball.XPos = Clamp(ball.XPos + dx, ball.Radius, _board.Width - ball.Radius);
+                ball.YPos = Clamp(ball.YPos + dy, ball.Radius, _board.Height - ball.Radius);
                 
                 BallMoved?.Invoke(this, new BallMovedEventArgs(ball.Id, ball.XPos, ball.YPos));
             }
@@ -55,6 +51,7 @@ namespace Logic
 
         public void Start(double intervalInMilliseconds)
         {
+            Console.WriteLine("Starting timer");
             _timer = new Timer(intervalInMilliseconds);
             _timer.Elapsed += (sender, e) => UpdatePositions(0.1);
             _timer.Start();
