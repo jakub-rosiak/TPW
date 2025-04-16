83eadb949dc1b67b141b0df43dd1984a264a3ae9
Jakub Rosiak <251620@edu.p.lodz.pl>
2025-04-16 21:59:01 +0200

    feat: create model classes

---
+++ Model/Interfaces/IBallSimulationModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Model.Interfaces;
+
+public interface IBallSimulationModel
+{
+    void CreateBalls(int count);
+    void StartSimulation(double intervalMilliseconds);
+    void StopSimulation();
+
+    ObservableCollection<BallDisplay> Balls { get; }
+
+    event EventHandler? BallsChanged;
+}
---
+++ Model/Models/BallDisplay.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+
+namespace Model;
+
+public class BallDisplay : INotifyPropertyChanged
+{
+    public int Id { get; }
+
+    private double _xPos;
+
+    public double XPos
+    {
+        get => _xPos;
+        set { _xPos = value;
+            OnPropertyChanged(nameof(XPos));
+        }
+    }
+    
+    private double _yPos;
+
+    public double YPos
+    {
+        get => _yPos;
+        set { _yPos = value; OnPropertyChanged(nameof(YPos)); }
+    }
+    
+    public double Radius { get; }
+
+    public BallDisplay(int id, double xPos, double yPos, double radius)
+    {
+        Id = id;
+        Radius = radius;
+        _xPos = xPos;
+        _yPos = yPos;
+    }
+    
+    public event PropertyChangedEventHandler? PropertyChanged;
+    protected void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+}
